927d7fe91ae7e9f7438bdfddaa104ad01a5d8645
ljw-linux2007 <71304327+ljw-linux2007@users.noreply.github.com>
2021-09-10 13:04:52 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/bash
+ #禁用firewalld
+ systemctl disable --now firewalld
+setenforce 0
+sed -i 's/enforcing/disabled/' /etc/selinux/config
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+#禁用swap
+swapoff -a
+ sed -i 's/.*swap.*/#&/' /etc/fstab
+function Check_linux_system(){
+    linux_version=`cat /etc/redhat-release`
+    if [[ ${linux_version} =~ "CentOS" ]];then
+        echo -e "\033[32;32m 系统为 ${linux_version} \033[0m \n"
+    else
+        echo -e "\033[32;32m 系统不是CentOS,该脚本只支持CentOS环境\033[0m \n"
+        exit 1
+    fi
+}
+ 
+yum -y install net-tools 
+function Set_hostname(){
+    if [ -n "$HostName" ];then
+      grep $HostName /etc/hostname && echo -e "\033[32;32m 主机名已设置，退出设置主机名步骤 \033[0m \n" && return
+      case $HostName in
+      help)
+        echo -e "\033[32;32m bash init.sh 主机名 \033[0m \n"
+        exit 1
+      ;;
+      *)
+        hostname $HostName
+        echo "$HostName" > /etc/hostname
+        
+      ;;
+      esac
+    else
+      echo -e "\033[32;32m 输入为空，请参照 bash init.sh 主机名 \033[0m \n"
+      exit 1
+    fi
+}
+ 
+function Install_depend_environment(){
+curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+
+yum clean all
+
+      rpm -qa | grep nfs-utils &> /dev/null && echo -e "\033[32;32m 已完成依赖环境安装，退出依赖环境安装步骤 \033[0m \n" && return
+yum install -y net-tools lftp rsync psmisc vim-enhanced tree lrzsz bash-completion iproute
+    yum install -y nfs-utils curl yum-utils device-mapper-persistent-data lvm2 net-tools conntrack-tools wget vim  ntpdate libseccomp libtool-ltdl telnet
+    yum install -y conntrack ipvsadm ipset jq iptables curl sysstat libseccomp && /usr/sbin/modprobe ip_vs 
+	wget   http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum -y install jq
+     echo -e "\033[32;32m 升级Centos7系统内核到5版本，解决Docker-ce版本兼容问题\033[0m \n"
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org && \
+    rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm && \
+    yum --disablerepo=\* --enablerepo=elrepo-kernel repolist && \
+    yum --disablerepo=\* --enablerepo=elrepo-kernel install -y kernel-ml.x86_64 && \
+    yum remove -y kernel-tools-libs.x86_64 kernel-tools.x86_64 && \
+    yum --disablerepo=\* --enablerepo=elrepo-kernel install -y kernel-ml-tools.x86_64 && \
+    grub2-set-default 0
+    modprobe br_netfilter
+    ls /proc/sys/net/bridge
+yum makecache
+
+cat > /etc/sysctl.d/k8s.conf << EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+fs.may_detach_mounts = 1
+vm.overcommit_memory=1
+vm.panic_on_oom=0
+fs.inotify.max_user_watches=89100
+fs.file-max=52706963
+fs.nr_open=52706963
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp.keepaliv.probes = 3
+net.ipv4.tcp_keepalive_intvl = 15
+net.ipv4.tcp.max_tw_buckets = 36000
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp.max_orphans = 327680
+net.ipv4.tcp_orphan_retries = 3
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.ip_conntrack_max = 65536
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.top_timestamps = 0
+net.core.somaxconn = 16384
+vm.swappiness=0
+net.ipv6.conf.all.disable_ipv6=1
+net.netfilter.nf_conntrack_max=2310720
+EOF
+echo "nf_conntrack" > /etc/modules-load.d/netfilter.conf
+
+sysctl --system
+
+} 
+function Install_docker(){
+   rpm -qa | grep docker && echo -e "\033[32;32m 已安装docker，退出安装docker步骤 \033[0m \n" && return
+    yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    yum makecache fast
+    yum -y install docker-ce-19.03.6 docker-ce-cli-19.03.6
+    # 设置 iptables file表中 FORWARD 默认链规则为 ACCEPT
+    sed  -i '/ExecStart=/i ExecStartPost=\/sbin\/iptables -P FORWARD ACCEPT' /usr/lib/systemd/system/docker.service
+    systemctl enable docker.service
+    systemctl start docker.service
+    systemctl stop docker.service
+    echo '{"registry-mirrors": ["https://0phn8t1c.mirror.aliyuncs.com","https://0c7f7dc46380109f0f6fc00b90ae4b80.mirror.swr.myhuaweicloud.com","https://hub-mirror.c.163.com"], "log-opts": {"max-size":"500m", "max-file":"3"},"exec-opts": ["native.cgroupdriver=systemd"]}' > /etc/docker/daemon.json
+    systemctl daemon-reload
+    systemctl start docker
+}
+# 设置vim格式
+
+ 
+# 初始化顺序
+HostName=$1
+Check_linux_system && \
+Set_hostname && \
+Install_depend_environment 
+#Install_docker
